5b0108fa7613a6f592dcf5d1d8c95b5be9719c30
str8builders <challis836@gmail.com>
2025-09-01 16:57:57 +1200

    Fix dropdown arrow in bottom navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Chrome as Home, FolderOpen, Users } from 'lucide-react-native';
+import { Home, FolderOpen, Users } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
